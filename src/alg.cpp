b2e3dddbf64f8f7cc1c58fb9a3d3f9ee9fcea1d9
DSFKnight <125854188+DSFKnight@users.noreply.github.com>
2025-02-16 16:14:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	while (b != 0) {
-		int temp = b;
-		b = a % b;
-		a = temp;
-	}
-	return a;
+	
+ while (b != 0) {
+   int temp = b;
+   b = a % b;
+   a = temp;
+ }
+ return a;
 }
